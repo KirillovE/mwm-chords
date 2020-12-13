9a653f97363d9b304b48a5b1884f0e5b07cb3679
Evgeniy <EVgeniy.Kirillov@live.ru>
2020-12-13 17:35:51 +0300

    Added network request

---
+++ MWM chors/TextualError.swift
@@ -0,0 +1,28 @@
+//
+//  TextualError.swift
+//  MWM chors
+//
+//  Created by Евгений Кириллов on 13.12.2020.
+//
+
+struct TextualError: Error {
+    private let text: String
+}
+
+extension TextualError: ExpressibleByStringLiteral {
+    init(stringLiteral value: String) {
+        self = TextualError(text: value)
+    }
+}
+
+extension TextualError: CustomStringConvertible {
+    var description: String {
+        text
+    }
+}
+
+extension TextualError: CustomDebugStringConvertible {
+    var debugDescription: String {
+        "\(loc("error")): \(description)"
+    }
+}
