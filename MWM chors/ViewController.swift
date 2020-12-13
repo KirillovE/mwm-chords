89a9bbac5d2c05f03ddca6e0077cc8548138a0ea
Evgeniy <EVgeniy.Kirillov@live.ru>
2020-12-13 15:43:50 +0300

    Added outlets

--- MWM chors/ViewController.swift
+++ MWM chors/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet weak var chordDataLabel: UILabel!
+    @IBOutlet weak var keyPicker: UIPickerView!
+    @IBOutlet weak var chordPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
