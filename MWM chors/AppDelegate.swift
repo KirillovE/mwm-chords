39bb2097eaac82c1df1725e31d3dff7fac1e8168
Evgeniy <EVgeniy.Kirillov@live.ru>
2020-12-13 15:09:57 +0300

    Initial setup

---
+++ MWM chors/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MWM chors
+//
+//  Created by Евгений Кириллов on 13.12.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+}
---
+++ MWM chors/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MWM chors
+//
+//  Created by Евгений Кириллов on 13.12.2020.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
